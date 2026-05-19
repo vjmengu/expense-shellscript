145f8629aacfcb892d817b88af7dbf06edc73187
vjmengu <igroweachday@gmail.com>
2026-05-19 22:41:57 +0530

    new addition

--- 01-mysql-server.sh
+++ 01-mysql-server.sh
@@ -16,21 +16,30 @@ validate (){
     fi
 }
 
-dnf list installed mysql-server
+mkdir -p /var/log/expense-log
+
+log_folder="/var/log/expense-log"
+logfile=$(echo $0)
+file_name=$(echo $logfile | cut -d "." -f1)
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+final_log="$log_folder/$file_name-$timestamp.log"
+echo "script started executing at $timestamp" &>>$final_log
+
+dnf list installed mysql-server &>>$final_log
 if [ $? -ne 0 ]
 then
-    dnf install mysql-server -y
+    dnf install mysql-server -y &>>$final_log
     validate $? "Mysql-server installation"
 else
     echo " mysql-server already installed "
 fi
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$final_log
 validate $? "mysql enable"
-systemctl start mysqld
+systemctl start mysqld &>>$final_log
 validate $? "mysql start"
 
-mysql -h 13.217.226.103 -u root -pExpenseApp@1 -e 'show databases;'
+mysql -h 13.217.226.103 -u root -pExpenseApp@1 -e 'show databases;' &>>$final_log
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ExpenseApp@1
