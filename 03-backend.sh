21d93648865b1405787da77c0689e749b08ddf9d
vjmengu <igroweachday@gmail.com>
2026-05-20 13:40:32 +0530

    new

--- 03-backend.sh
+++ 03-backend.sh
@@ -29,25 +29,25 @@ validate(){
     fi
 }
 
-echo " The script execution started at....$timestamp" >>&$log_file
+echo " The script execution started at....$timestamp" &>>$log_file
 check
 
-dnf installed nodejs >>&$log_file
+dnf installed nodejs &>>$log_file
 if [ $? -ne 0 ]
 then
-    dnf module disable nodejs -y >>&$log_file
+    dnf module disable nodejs -y &>>$log_file
     validate $? "nodejs disabling"
-    dnf module  enable nodejs:20 -y >>&$log_file
-    dnf install nodejs -y >>&$log_file
+    dnf module  enable nodejs:20 -y &>>$log_file
+    dnf install nodejs -y &>>$log_file
     validate $? "nodejs installation"
 else
     echo -e " Nodjs already installed...$Y Skipping $N"
 fi
 
-id expense >>&$log_file
+id expense &>>$log_file
 if [ $? -ne 0 ]
 then
-    useradd expense >>&$log_file
+    useradd expense &>>$log_file
     validate $? "expense user adding"
 else
     echo -e "Expense User already existing...$Y Skipping $N"
@@ -55,33 +55,33 @@ fi
 
 mkdir -p /app
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip >>&$log_file
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$log_file
 validate $? "Downloading backend"
 
-cd /app >>&$log_file
+cd /app &>>$log_file
 
-rm -rf /app/* >>&$log_file
+rm -rf /app/* &>>$log_file
 
-unzip /tmp/backend.zip >>&$log_file
+unzip /tmp/backend.zip &>>$log_file
 validate $? "unziping backend"
 
-npm install >>&$log_file
+npm install &>>$log_file
 validate $? "npm install"
 
-cp /home/ec2-user/expense-shellscript/backend.service /etc/systemd/system/backend.service >>&$log_file
+cp /home/ec2-user/expense-shellscript/backend.service /etc/systemd/system/backend.service &>>$log_file
 
-dnf install mysql >>&$log_file
+dnf install mysql &>>$log_file
 validate $? "mysql installation"
 
-mysql -h mysql.gt650.online -u root -pExpenseApp@1 < /app/schema/backend.sql >>&$log_file
+mysql -h mysql.gt650.online -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 validate $? "schema setting to mysql data-base"
 
-systemctl daemon-reload >>&$log_file
+systemctl daemon-reload &>>$log_file
 validate $? "backend service reloaded"
 
 
-systemctl enable backend >>&$log_file
+systemctl enable backend &>>$log_file
 validate $? "backend enabling"
 
-systemctl restart backend >>&$log_file
+systemctl restart backend &>>$log_file
 validate $? "backend restart"
